4ecf5b029593946713d886bd9f88a4a914cfd10c
weidian <youzhijun@weidian.com>
2019-01-01 17:49:56 +0800

    抓取歌手数据，转化数据格式

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,95 @@
 <template>
   <div class="singer">
-    我是歌手
+    
   </div>
 </template>
 
 <script>
-export default {
+import { getSingerList } from 'api/singer.js'
+import {ERR_OK} from 'api/config.js'
+import Singer from 'common/js/singer.js'
 
+const HOT_NAME = '热门'
+const HOT_LIST_LENGTH = 10
+export default {
+  name: 'singer',
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    // 获取歌手数据
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if(res.code === ERR_OK) {
+          this.singers = res.data && res.data.list || []
+          console.log(this._normalizeSinger(this.singers))
+        }
+      })
+    },
+    // 重新整合歌手数据
+    _normalizeSinger(singers) {
+      // 数据结构
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      // 生成热门和字母数据结构
+      singers.forEach((item, index) => {
+        // 热门
+        if(index < HOT_LIST_LENGTH) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        // 字母
+        let key = item.Findex
+        if(!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+
+      // 处理map数据，变为有序列表
+      let hot = []
+      let ret = []
+      for(let key in map) {
+        let val = map[key]
+        if(val.title.match(/a-zA-Z/)) {
+          ret.push(val)
+        } else if(val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      // 排序字母数据, a~z
+      ret.sort((a, b) => {
+        return a.title.charCodeAt[0] - b.title.charCodeAt[0]
+      })
+      console.log('map', map)
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+  @import "../../common/stylus/variable.styl"
+  .singer
+    position fixed
+    top 88px
+    bottom 0
+    width 100%
 </style>
