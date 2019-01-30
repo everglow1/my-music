01aac826f3db6756be6353e321534f9aff752c67
weidian <youzhijun@weidian.com>
2019-01-30 17:47:47 +0800

    标题fixed效果

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -27,14 +27,24 @@
         >{{item}}</li>
       </ul>
     </div>
+    <!-- 固定fixed头 -->
+    <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+      <h1 class="fixed-title">{{fixedTitle}}</h1>
+    </div>
+    <!-- loading -->
+    <div v-show="!data.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll.vue'
+import Loading from 'base/loading/load.vue'
 import { getData } from 'common/js/dom.js'
 
 const ANCHOR_HEIGHT = 18    // 单个字母的高度
+const TITLE_HEIGHT = 30
 export default {
   name: 'listview',
   props: {
@@ -49,11 +59,13 @@ export default {
     return {
       // 观察的y值和索引
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1  // 头部固定标题对比值
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   },
   created() {
     // 存放touch值
@@ -71,6 +83,13 @@ export default {
         return group.title.substr(0, 1)
       })
       return letters
+    },
+    // 固定头部
+    fixedTitle() {
+      if(this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   methods: {
@@ -107,7 +126,6 @@ export default {
       } else if(index > this.listHeight.length - 2) {  // 滚动到最底部
         index = this.listHeight.length - 2
       }
-      console.log('index', index)
       this.scrollY = -this.listHeight[index]
       this.$refs.listView.scrollToElement(this.$refs.listgroup[index], 0)   // 滚动到对应索引值的分组
     },
@@ -145,11 +163,21 @@ export default {
         let height2 = listHeight[i+1]
         if(-newY >= height1 && -newY < height2) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
       // 当滚动到底部，-newY大于最后一个元素的上限。
       this.currentIndex = listHeight.length - 2
+    },
+    // 固定标题偏移
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT ) ? newVal - TITLE_HEIGHT : 0
+      if(this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px, 0)`
     }
   }
 }
@@ -185,23 +213,40 @@ export default {
           margin-left: 20px
           color: $color-text-l
           font-size: $font-size-medium
-  .list-shortcut
-    position: absolute
-    z-index: 30
-    right: 0
-    top: 50%
-    transform: translateY(-50%)
-    width: 20px
-    padding: 20px 0
-    border-radius: 10px
-    text-align: center
-    background: $color-background-d
-    font-family: Helvetica
-    .item
-      padding: 3px
-      line-height: 1
-      color: $color-text-l
-      font-size: $font-size-small
-      &.current
-        color $color-theme
+    .list-shortcut
+      position: absolute
+      z-index: 30
+      right: 0
+      top: 50%
+      transform: translateY(-50%)
+      width: 20px
+      padding: 20px 0
+      border-radius: 10px
+      text-align: center
+      background: $color-background-d
+      font-family: Helvetica
+      .item
+        padding: 3px
+        line-height: 1
+        color: $color-text-l
+        font-size: $font-size-small
+        &.current
+          color $color-theme
+    .list-fixed
+      position absolute
+      top 0
+      left 0
+      width 100%
+      .fixed-title
+        height: 30px
+        line-height: 30px
+        padding-left: 20px
+        font-size: $font-size-small
+        color: $color-text-l
+        background: $color-highlight-background
+    .loading-container
+      position: absolute
+      width: 100%
+      top: 50%
+      transform: translateY(-50%)
 </style>
