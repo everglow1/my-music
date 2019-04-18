83802c128158a9510c2426f8f024bacdad29c33d
weidian <youzhijun@weidian.com>
2019-04-18 23:28:11 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,20 @@
-/**
- * vuex入口文件,负责vuex初始化，暴露出去。
- */
-
 import Vue from 'vue'
 import Vuex from 'vuex'
-// vuex下的一个工具，每次mutations改变state时，会console一个日志
+import * as actions from './actions'
+import * as getters from './getters'
+import state from './state'
+import mutations from './mutations'
 import createLogger from 'vuex/dist/logger'
 
-import state from './state.js'
-import mutations from './mutations.js'
-import * as getters from './getters.js'
-import * as actions from './actions.js'
-
-// 注册vuex
 Vue.use(Vuex)
 
-// 报错处理，假如不是mutation修改的store，回报错。 （不建议在线上环境使用）
 const debug = process.env.NODE_ENV !== 'production'
 
-// 输出一个Vuex的实例（有点像单例模式）
 export default new Vuex.Store({
+  actions,
+  getters,
   state,
   mutations,
-  getters,
-  actions,
-  // 辅助开发的一些调试啥的
   strict: debug,
   plugins: debug ? [createLogger()] : []
 })
